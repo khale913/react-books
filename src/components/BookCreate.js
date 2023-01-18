0862959a4ba843a5566bb7b5c5c8c6501a44b023
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-01-17 20:16:05 -0800

    form submission and page layout

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,27 @@
-function BookCreate() {
-    return <div>Book Create</div>
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">Title</label>
+        <input type="text" value={title} onChange={handleChange} />
+        <button>Create!</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
