493842c95aae72b10664499011739e3de8f69ffb
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-01-18 19:28:32 -0800

    render books and delte

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 999), title: title },
+    ];
+    setBooks(updatedBooks);
+    console.log(books);
+
     console.log("Need to add book with:", title);
   };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById}></BookList>
       <BookCreate onCreate={createBook}></BookCreate>
     </div>
   );
